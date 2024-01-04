ef20868a59c8dfecba64e86159c639107d45ace8
Wes Koerber <wes@weskoerber.com>
2024-01-03 22:57:36 -0500

    chore: update tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.rs", "./static/**/*.{html,js}"],
+  content: [
+    "./src/**/*.rs",
+    "./static/**/*.{html,js}",
+    "./templates/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
